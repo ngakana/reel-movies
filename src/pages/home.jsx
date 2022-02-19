1385ca00617f29d66cb67fd1d4dd1d5050ed15c5
ngakana <nlsalemane@gmail.com>
2022-02-19 13:58:03 +0200

    ✨ 💄 polish homepage

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,20 @@
+import { Link } from "react-router-dom";
+
 function Home() {
-  return <h1>Homepage</h1>;
+  return (
+    <div className="home-page">
+      <h1>ReelMovies</h1>
+      {/* <Branding /> */}
+      <div className="links">
+        <Link to="/movies">
+          <span>Browse movies</span>
+        </Link>
+        <Link to="/library">
+          <span>Library</span>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
